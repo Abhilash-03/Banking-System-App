b8b5dd94576aad8d7ec6da4e0341f769733867b4
Abhilash <abhilashjha335@gmail.com>
2024-08-16 22:22:44 +0530

    implement home page with right and left sidebar and chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+  const loggedIn = { firstName: 'Abhilash', lastName: 'Jha', email: 'contact@horizon.com'};
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1353.32}
+          />
+        </header>
+        RECENT TRANSACTION
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 124.54}, {currentBalance: 2353.33}]}
+      />
+    </section>
   )
 }
 
